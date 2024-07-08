0fa905cb798d677eb546ec358b2914f7223e0056
Oluwatosin Ishmeal <olaleyeoluwatosin@gmail.com>
2024-07-08 23:03:09 +0000

    added functionality to mark task as done and to display in the done tab

--- todo_main/views.py
+++ todo_main/views.py
@@ -5,9 +5,11 @@ from todos.models import Task
 
 
 def home(request):
-    # order_ by wont work now due to the issue of datetimefield not beingn string
+    # order_ by wont work now due to the issue of datetimefield not string
     tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
+    completed_tasks = Task.objects.filter(is_completed=True)
     context = {
-        'tasks' : tasks
+        'tasks' : tasks,
+        'completed_tasks' : completed_tasks
     }
     return render(request, 'home.html', context)
